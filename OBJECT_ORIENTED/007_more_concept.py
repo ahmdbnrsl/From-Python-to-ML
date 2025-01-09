5ce7fd527b15b5e7711b1241bf4714a75b2a63b7
abrazax56 <bruceeeelyy@gmail.com>
2025-01-09 07:39:25 +0700

    OOP: more concept

--- OBJECT_ORIENTED/007_more_concept.py
+++ OBJECT_ORIENTED/007_more_concept.py
@@ -13,3 +13,18 @@ class Singleton:
 Nama = Singleton("Ahmad")
 print(Nama.nama)
 print(vars(Nama))
+
+class Singleton2:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            print("Membuat instance baru.")
+            cls._instance = super().__new__(cls)
+        else:
+            print("Menggunakan instance yang sudah ada.")
+        return cls._instance
+
+obj1 = Singleton2()
+obj2 = Singleton2()
+print(obj1 is obj2)  # Output: True
