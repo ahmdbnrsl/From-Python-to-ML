c8e781c49484cc8888c2f4634bc47b715908c866
ahmdbnrsl <bruceeeelyy@gmail.com>
2025-02-09 09:06:06 +0700

    Basic Math(Linear Algebra): Singular Value Decomposition && Image Compresser

--- BASIC_MATH/LINEAR_ALGEBRA/image_compreser.py
+++ BASIC_MATH/LINEAR_ALGEBRA/image_compreser.py
@@ -2,18 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from skimage import data, img_as_float
 
-# Muat gambar contoh (grayscale) dari skimage
 img = img_as_float(data.camera())
 
-# Lakukan SVD pada gambar
 U, S, Vt = np.linalg.svd(img, full_matrices=False)
 
-# Pilih hanya k singular values teratas (misalnya, k = 50)
 k = 50
 S_k = np.diag(S[:k])
 img_compressed = U[:, :k] @ S_k @ Vt[:k, :]
 
-# Visualisasikan gambar asli dan terkompresi
 fig, axes = plt.subplots(1, 2, figsize=(12, 6))
 axes[0].imshow(img, cmap='gray')
 axes[0].set_title("Gambar Asli")
